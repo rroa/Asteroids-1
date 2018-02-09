315c8972fa75534d1ca609a358c2f4fea4ec2db1
Raúl Roa <raul.roa@gmail.com>
2018-02-09 10:19:12 -0400

    Assignment 3 (#5)
    * Added function prototypes for MoveForward, RotateLeft and RotateRight
    
    * Addressed PR comments
    
    * Added new Math filter (within Engine
    ) and moved Math related types into it.
    
    * Added Vector4 type.
    
    * Added world warping.
    
    * Changed shape of the ship, from square to triangle.

--- src/BoilerPlate/Player.cpp
+++ src/BoilerPlate/Player.cpp
@@ -3,10 +3,24 @@
 
 namespace Game
 {
-	Player::Player()
+	inline float wrap(float x, float min, float max)
+	{
+		if (x < min) return max - (min - x);
+		if (x > max) return min + (x - max);
+		return x;
+	}
+
+	Player::Player(int width, int height)
 	{
 		m_position =
 			new Engine::Math::Vector2(Engine::Math::Vector2::Origin);
+
+		// TODO: RR: Ewww! Move this out!
+		m_maxWidth = width / 2.0f;
+		m_minWidth = -width / 2.0f;
+
+		m_maxHeight = height / 2.0f;
+		m_minHeight = -height / 2.0f;
 	}
 
 	void Player::Update()
@@ -14,8 +28,11 @@ namespace Game
 
 	void Player::Move(const Engine::Math::Vector2& unit)
 	{
-		m_position->x += unit.x;
-		m_position->y += unit.y;
+		float x = m_position->x + unit.x;
+		float y = m_position->y + unit.y;
+		
+		m_position->x = wrap(x, m_minWidth, m_maxWidth);
+		m_position->y = wrap(y, m_minHeight, m_maxHeight);
 	}
 
 	void Player::Render()
@@ -27,10 +44,11 @@ namespace Game
 
 		// Draws a square
 		glBegin(GL_LINE_LOOP);
-			glVertex2f(50.0, 50.0);
-			glVertex2f(50.0, -50.0);
-			glVertex2f(-50.0, -50.0);
-			glVertex2f(-50.0, 50.0);
+			glVertex2f(0.0f, 20.0f);
+			glVertex2f(12.0f, -10.0f);
+			glVertex2f(6.0f, -4.0f);
+			glVertex2f(-6.0f, -4.0f);
+			glVertex2f(-12.0f, -10.0f);
 		glEnd();
 	}
 }
